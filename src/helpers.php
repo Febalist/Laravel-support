50a934a4a139149ffada19a2103f55d6d1591457
Febalist <febalist@gmail.com>
2017-02-10 17:47:52 +0300

    Style fix

--- src/helpers.php
+++ src/helpers.php
@@ -267,7 +267,7 @@ if (!function_exists('isAssoc')) {
         if ([] === $array) {
             return false;
         }
-        
+
         return array_keys($array) !== range(0, count($array) - 1);
     }
 }
