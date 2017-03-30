36b3bde2540aee1de58b785f65a37295bd9902b3
Michael Papin <febalist@gmail.com>
2017-03-30 16:44:42 +0000

    Apply fixes from StyleCI

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -12,10 +12,10 @@ use Whoops\Run as Whoops;
 
 class Handler extends ExceptionHandler
 {
-    public function renderForConsole ($output, Exception $e)
+    public function renderForConsole($output, Exception $e)
     {
         if ($this->whoops()) {
-            $whoops = new Whoops;
+            $whoops = new Whoops();
             $whoops->pushHandler(new PlainTextHandler());
             $whoops->writeToOutput(true);
             $whoops->handleException($e);
@@ -23,23 +23,24 @@ class Handler extends ExceptionHandler
         parent::renderForConsole($output, $e);
     }
 
-    protected function convertExceptionToResponse (Exception $e)
+    protected function convertExceptionToResponse(Exception $e)
     {
         if (!$this->isHttpException($e) && $this->whoops()) {
-            $whoops = new Whoops;
+            $whoops = new Whoops();
             if (request()->expectsJson()) {
                 $whoops->pushHandler(new JsonResponseHandler());
             } else {
                 $whoops->pushHandler(new PrettyPageHandler());
             }
+
             return new Response($whoops->handleException($e), 500);
         }
+
         return parent::convertExceptionToResponse($e);
     }
 
-    protected function whoops ()
+    protected function whoops()
     {
         return config('app.debug') && class_exists(Whoops::class);
     }
 }
-
