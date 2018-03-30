f7333699d5b60bd54741efaff6f1617d79f6071f
febalist <febalist@gmail.com>
2018-03-30 13:29:38 +0300

    ide_helper HigherOrderCollectionProxy

--- _ide_helper.php
+++ _ide_helper.php
@@ -10,3 +10,45 @@ namespace Illuminate\Database\Schema {
 
     }
 }
+
+namespace Illuminate\Database\Eloquent {
+
+    use Illuminate\Support\HigherOrderCollectionProxy as Proxy;
+
+    /**
+     * @property-read Proxy average
+     * @property-read Proxy avg
+     * @property-read Proxy contains
+     * @property-read Proxy each
+     * @property-read Proxy every
+     * @property-read Proxy filter
+     * @property-read Proxy first
+     * @property-read Proxy flatMap
+     * @property-read Proxy groupBy
+     * @property-read Proxy keyBy
+     * @property-read Proxy map
+     * @property-read Proxy max
+     * @property-read Proxy min
+     * @property-read Proxy partition
+     * @property-read Proxy reject
+     * @property-read Proxy sortBy
+     * @property-read Proxy sortByDesc
+     * @property-read Proxy sum
+     * @property-read Proxy unique
+     */
+    class Collection extends \Illuminate\Support\Collection
+    {
+
+    }
+}
+
+namespace Illuminate\Support {
+
+    use Illuminate\Database\Eloquent\Model;
+
+    /** @mixin Model */
+    class HigherOrderCollectionProxy
+    {
+
+    }
+}
