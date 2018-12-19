7406cdb6f3547a309c7cd825f719dbd221a4ae6d
febalist <febalist@gmail.com>
2018-12-19 19:17:35 +0300

    Collection macros

--- src/SupportServiceProvider.php
+++ src/SupportServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Febalist\Laravel\Support;
 
 use Blade;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 use Mingalevme\Illuminate\Lock\LaravelLockServiceProvider;
 use Validator;
@@ -36,6 +37,61 @@ class SupportServiceProvider extends ServiceProvider
 
             return preg_match('/^[-+]?\d+[.,]?\d*$/', $value);
         });
+
+        if (!Collection::hasMacro('append')) {
+            Collection::macro('append', function ($source) {
+                foreach ($source as $item) {
+                    $this->push($item);
+                }
+
+                return $this;
+            });
+        }
+
+        if (!Collection::hasMacro('without')) {
+            Collection::macro('without', function ($values) {
+                if ($values instanceof Collection) {
+                    $values = $values->all();
+                } elseif (!is_array($values)) {
+                    $values = func_get_args();
+                }
+
+                $items = array_without($this->all(), $values);
+
+                return collect($items);
+            });
+        }
+
+        if (!Collection::hasMacro('remove')) {
+            Collection::macro('remove', function ($values) {
+                if ($values instanceof Collection) {
+                    $values = $values->all();
+                } elseif (!is_array($values)) {
+                    $values = func_get_args();
+                }
+
+                $collection = collect($this->all())->without($values);
+
+                $this->replace($collection);
+
+                return $this;
+            });
+        }
+
+        if (!Collection::hasMacro('replace')) {
+            Collection::macro('replace', function ($values) {
+                if ($values instanceof Collection) {
+                    $values = $values->all();
+                } elseif (!is_array($values)) {
+                    $values = func_get_args();
+                }
+
+                $this->splice(0);
+                $this->append($values);
+
+                return $this;
+            });
+        }
 
         $this->loadTranslationsFrom(__DIR__.'/../lang', 'support');
     }
