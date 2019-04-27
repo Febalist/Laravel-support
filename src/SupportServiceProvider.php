fcf75daf63f74193dcec672fbe1b2c44e7b56fee
febalist <febalist@gmail.com>
2019-04-27 21:59:26 +0300

    Boot methods

--- src/SupportServiceProvider.php
+++ src/SupportServiceProvider.php
@@ -7,7 +7,6 @@ use Illuminate\Queue\Events\JobProcessing;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\ServiceProvider;
-use Mingalevme\Illuminate\Lock\LaravelLockServiceProvider;
 use Validator;
 
 class SupportServiceProvider extends ServiceProvider
@@ -19,26 +18,78 @@ class SupportServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        javascript([
-            'env' => config('app.env'),
-            'debug' => config('app.debug'),
-            'version' => config('version.number'),
-        ]);
+        $this->bootJavascript();
+        $this->bootSentry();
+        $this->bootCarbon();
+        $this->bootValidator();
+        $this->bootCollections();
+        $this->bootBlade();
+        $this->bootQueue();
 
-        Sentry::instance()->boot();
+        $this->loadTranslationsFrom(__DIR__.'/../lang', 'support');
+    }
 
-        \Carbon\Carbon::useMonthsOverflow(false);
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        Macro::register();
+
+        $this->mergeConfigFrom(__DIR__.'/../config/javascript.php', 'javascript');
+        $this->mergeConfigFrom(__DIR__.'/../config/version.php', 'version');
+        $this->mergeConfigFrom(__DIR__.'/../config/sentry.php', 'sentry');
+
+        require_once __DIR__.'/helpers.php';
+    }
+
+    protected function bootBlade()
+    {
+        $this->publishes([__DIR__.'/../views' => resource_path('views/vendor/support')]);
+        $this->loadViewsFrom(__DIR__.'/../views', 'support');
+
+        Blade::component('support::components.alert', 'alert');
+        Blade::component('support::components.group', 'group');
+        Blade::component('support::components.delete', 'delete');
 
         Blade::if('debug', function () {
             return config('app.debug');
         });
+    }
 
-        Validator::extend('float', function ($attribute, $value, $parameters, $validator) {
-            $value = preg_replace('/\s/', '', $value);
+    protected function bootQueue()
+    {
+        Queue::before(function (JobProcessing $event) {
+            $class = $event->job->resolveName();
 
-            return preg_match('/^[-+]?\d+[.,]?\d*$/', $value);
+            $this->app->bindMethod("$class@handle", function ($job, $app) {
+                if (method_exists($job, 'beforeHandle')) {
+                    $job->beforeHandle();
+                }
+
+                $job->handle();
+
+                if (method_exists($job, 'afterHandle')) {
+                    $job->afterHandle();
+                }
+            });
         });
+    }
 
+    protected function bootSentry()
+    {
+        Sentry::instance()->boot();
+    }
+
+    protected function bootCarbon()
+    {
+        \Carbon\Carbon::useMonthsOverflow(false);
+    }
+
+    protected function bootCollections()
+    {
         if (!Collection::hasMacro('append')) {
             Collection::macro('append', function ($source) {
                 foreach ($source as $item) {
@@ -93,56 +144,23 @@ class SupportServiceProvider extends ServiceProvider
                 return $this;
             });
         }
-
-        $this->bootBlade();
-
-        $this->bootQueue();
-
-        $this->loadTranslationsFrom(__DIR__.'/../lang', 'support');
     }
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
+    protected function bootValidator()
     {
-        Macro::register();
+        Validator::extend('float', function ($attribute, $value, $parameters, $validator) {
+            $value = preg_replace('/\s/', '', $value);
 
-        $this->mergeConfigFrom(__DIR__.'/../config/javascript.php', 'javascript');
-        $this->mergeConfigFrom(__DIR__.'/../config/version.php', 'version');
-        $this->mergeConfigFrom(__DIR__.'/../config/sentry.php', 'sentry');
-
-        require_once __DIR__.'/helpers.php';
-    }
-
-    protected function bootBlade()
-    {
-        $this->publishes([__DIR__.'/../views' => resource_path('views/vendor/support')]);
-        $this->loadViewsFrom(__DIR__.'/../views', 'support');
-
-        Blade::component('support::components.alert', 'alert');
-        Blade::component('support::components.group', 'group');
-        Blade::component('support::components.delete', 'delete');
-    }
-
-    protected function bootQueue()
-    {
-        Queue::before(function (JobProcessing $event) {
-            $class = $event->job->resolveName();
-
-            $this->app->bindMethod("$class@handle", function ($job, $app) {
-                if (method_exists($job, 'beforeHandle')) {
-                    $job->beforeHandle();
-                }
-
-                $job->handle();
-
-                if (method_exists($job, 'afterHandle')) {
-                    $job->afterHandle();
-                }
-            });
+            return preg_match('/^[-+]?\d+[.,]?\d*$/', $value);
         });
+    }
+
+    protected function bootJavascript()
+    {
+        javascript([
+            'env' => config('app.env'),
+            'debug' => config('app.debug'),
+            'version' => config('version.number'),
+        ]);
     }
 }
